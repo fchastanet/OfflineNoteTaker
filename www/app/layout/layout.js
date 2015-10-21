a0b80060cf920aa1a32f24f1822a25f9b059d46e
Francois Chastanet <fchastanet@gmail.com>
2015-10-21 22:01:29 +0200

    Move toggleOnline button into layout template

--- www/app/layout/layout.js
+++ www/app/layout/layout.js
@@ -4,18 +4,44 @@
     angular
         .module('app.layout')
         .controller('LayoutController', LayoutController)
-        .controller('MenuController', MenuController)
+        .controller('LayoutMenuController', LayoutMenuController)
     ;
 
     /* @ngInject */
-    function LayoutController($scope, logger) {
+    function LayoutController($rootScope, $scope, nodeService, logger, toastrWrapper) {
+        /*jshint validthis: true */
+        $scope.online = false;
+        $scope.toggleOnline = function() {
+            nodeService.toggleOnline();
+        };
+
+        /*jshint validthis: true */
+        $rootScope.$on('sync.onlineState', function(event, data) {
+            $scope.online = data.state;
+            if (data.state) {
+                toastrWrapper.info('aplication online');
+            } else {
+                toastrWrapper.info('aplication offline');
+            }
+        });
+        $scope.syncStatus = {
+            'code': null,
+            'label': ''
+        };
+        $rootScope.$on('sync.syncState', function(event, data) {
+            $scope.syncStatus.code = data.state;
+            if (data.state === 'paused') {
+                $scope.syncStatus.label = 'synchronisation en pause';
+            } else {
+                $scope.syncStatus.label = 'synchronisation active';
+            }
+        });
+    }
+
+     /* @ngInject */
+    function LayoutMenuController($scope, logger) {
         /*jshint validthis: true */
 
     }
 
-    /* @ngInject */
-    function MenuController($scope, logger) {
-        /*jshint validthis: true */
-
-    }
 })();
