dc026d8a6cb94449423ea6604aeaf91266b48731
François Chastanet <fchastanet@gmail.com>
2015-10-26 14:55:50 +0100

    design document added enum fields check

--- couchdbDesignDocuments/node.js
+++ couchdbDesignDocuments/node.js
@@ -23,24 +23,38 @@
             
             function requireType(field, fieldType, message) {
                 message = message || 'Field '+field+' must be ' + fieldType;
-                if (typeof newDoc[field] !== fieldType) throw ({forbidden: message});
+                if (typeof newDoc[field] !== fieldType) {
+                    throw ({forbidden: message});
+                }
             };            
 
-            requireType('type', 'string'); //todo enum
+            requireType('type', 'string'); 
             requireType('creationDate', 'string'); //todo format
             requireType('title', 'string'); //todo not empty, ensure unique
             requireType('author', 'string'); //todo not empty 
-            requireType('contentFormat', 'string'); //todo enum
+            requireType('contentFormat', 'string');
             requireType('content', 'string');
             requireType('fields', 'object');
             //todo validate each field 
 
-            function unchanged(field) { 
-                if (oldDoc && toJSON(oldDoc[field]) != toJSON(newDoc[field]))
-                  throw({forbidden : 'Field can\'t be changed: ' + field});
+            function unchanged(field, message) { 
+                message = message || 'Field can\'t be changed: ' + field;
+                if (oldDoc && toJSON(oldDoc[field]) != toJSON(newDoc[field])) {
+                  throw({forbidden : message});
+                }
             }
             unchanged('creationDate'); 
             unchanged('_id');
+
+            function enumField(field, valueList, message) {
+                message = message || 'Field '+field+' value must be one of ' + valueList.join();
+                if (typeof newDoc[field] !== 'undefined' && valueList.indexOf(newDoc[field]) === -1) {
+                  throw({forbidden : message});
+                }
+            }
+            enumField('type', ['lyrics']);
+            enumField('contentFormat', ['makdown', 'html', 'plainText']);
+
         }/*</stringify>*/
     ,'views': {
         'by_title': {
